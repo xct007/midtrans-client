5e3ac48e5d2ea5fa639e534361ac265f2015ee57
FrierenDv <hackerxct007@gmail.com>
2024-11-16 11:17:13 +0700

    test(payment-api): add tests for card registration, GoPay account creation, and BIN metadata retrieval

--- __tests__/core.payment-api.test.ts
+++ __tests__/core.payment-api.test.ts
@@ -53,4 +53,47 @@ describe("Core API (Payment API)", () => {
 		const response = await midtransClient.Core.cancel(order_id);
 		expect(response.transaction_status).toBe("cancel");
 	});
+
+	it("should be able to register card.", async () => {
+		const response = await midtransClient.Core.registerCard({
+			card_number: "4811111111111114",
+			card_exp_month: "12",
+			card_exp_year: "2025",
+		});
+		expect(response.status_code).toBe("200");
+		expect(response.saved_token_id).toBeDefined();
+		expect(response.transaction_id).toBeDefined();
+		expect(response.masked_card).toBeDefined();
+	});
+
+	it("should be able to create pay account (GoPay).", async () => {
+		const response = await midtransClient.Core.createGoPayAccount({
+			payment_type: "gopay",
+			gopay_partner: {
+				phone_number: "81212345678",
+				country_code: "62",
+			},
+		});
+		// because we are using sandbox and fake phone number
+		// the response will be failed
+		expect(response.status_code).toBe("202");
+		expect(response.payment_type).toBe("gopay");
+	});
+
+	it("should be able to get BIN API (Card Payment).", async () => {
+		const bin = "45563300";
+		const response = await midtransClient.Core.getBinMetadata(bin);
+		expect(response.data).toBeDefined();
+		const { data } = response;
+		expect(data.registration_required).toBeNull();
+		expect(data.country_name).toBeDefined();
+		expect(data.country_code).toBeDefined();
+		expect(data.channel).toBeDefined();
+		expect(data.brand).toBeDefined();
+		expect(data.bin_type).toBeDefined();
+		expect(data.bin_class).toBeDefined();
+		expect(data.bin).toBe(bin);
+		expect(data.bank_code).toBeDefined();
+		expect(data.bank).toBeDefined();
+	});
 });
