2c4ffd6bb3317951a1838e0e8b7ef600e3c45e1c
FrierenDv <hackerxct007@gmail.com>
2024-11-16 16:43:01 +0700

    refactor(error): simplify error handling by consolidating error classes and updating MidtransError structure

--- src/error.ts
+++ src/error.ts
@@ -1,36 +1,9 @@
 /* eslint-disable no-unused-vars */
-export interface IMidtransHttpError {
-	status_code?: string;
-	status_message?: string;
-	id?: string;
-	error_messages?: string[];
-}
-
-export class HttpClientError extends Error {
-	constructor(
-		message: string,
-		public readonly error?: unknown
-	) {
-		super(message);
-		this.name = "HttpClientError";
-	}
-}
-
-export class HttpStatusError extends HttpClientError {
-	constructor(
-		message: string,
-		public readonly statusCode: number,
-		error?: IMidtransHttpError
-	) {
-		super(message, error);
-		this.name = "HttpStatusError";
-	}
-}
-
 export class MidtransError extends Error {
 	constructor(
 		message: string,
-		public readonly _error?: unknown
+		public readonly error?: unknown,
+		public readonly statusCode?: number
 	) {
 		super(message);
 		this.name = "MidtransError";
